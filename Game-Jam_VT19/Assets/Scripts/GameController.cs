aafa34187c86517a842b19b4afa72db556be776d
TinyKenny <Jonathan@rawet.se>
2019-03-29 23:46:56 +0100

    started gamecontroller

--- Game-Jam_VT19/Assets/Scripts/GameController.cs
+++ Game-Jam_VT19/Assets/Scripts/GameController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private GameObject[] pipes;
+
+    //private Random rngSus = new Random();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,15 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int pipeToPickIndex = Random.Range(0, pipes.Length - 1);
+
+        GameObject pipeToPick = pipes[pipeToPickIndex];
+
+        Transform[] pipeEntrances = pipeToPick.GetComponentsInChildren<Transform>();
+
+        int entranceToPickIndex = Random.Range(0, pipeEntrances.Length - 1);
+
+        //GameObject entranceToPick
+
     }
 }
