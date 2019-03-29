d7869f975038f7d1e6d8dcaeadf045816e139ae9
TinyKenny <Jonathan@rawet.se>
2019-03-29 17:52:24 +0100

    base project!

---
+++ Game-Jam_VT19/Assets/Scripts/ShittyCameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShittyCameraScript : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position - (player.transform.forward * 20.0f);
+        //transform.LookAt(player.transform);
+        transform.rotation = player.transform.rotation;
+    }
+}
---
+++ Game-Jam_VT19/Assets/Scripts/ShittyPlayerControls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShittyPlayerControls : MonoBehaviour
+{
+    private float moveSpeedMod = 1.5f;
+    private float rotationSpeedMod = 20.0f;
+
+    /* 
+     * movement speed up
+     * movement speed down
+     * projectile speed up
+     * projectile speed down
+     * rotation speed up
+     * rotation speed down
+     * fire rate up
+     * fire rate down
+     * invert controls
+     * auto aim
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float rotateX = Input.GetAxisRaw("Pitch");
+        float rotateY = Input.GetAxisRaw("Yaw");
+        float rotateZ = Input.GetAxisRaw("Roll");
+
+        //Debug.Log(Input.GetAxisRaw("Yaw"));
+
+        transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * rotationSpeedMod * Time.deltaTime); // z = Input.GetAxisRaw("Horizontal")
+
+
+        transform.Translate(transform.forward * moveSpeedMod * Time.deltaTime);
+    }
+}
